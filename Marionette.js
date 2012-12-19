39fea77522f60ec133e32a3b17d4c6ef51451fd9
Doug Wollison <doug@ninesides.com>
2012-12-19 11:49:14 -0500

    Initial Commit: PHP & JS files

---
+++ Marionette.js
@@ -0,0 +1,291 @@
+//Marionette Javascript API
+var Marionette = {
+	//Script script
+	script: '',
+
+	//Session Key
+	sessionKey: null,
+
+	//The update interval
+	updateInterval: 1000,
+
+	//Default data to send
+	defaultData: null,
+
+	//Send Count
+	sendCount: 0,
+
+	//Logging Boolean
+	logging: false,
+
+	//Action Log
+	actionLog: [],
+
+	//Retry counter, limit and pace
+	tries: 0,
+	//maxTries: 10,
+	retryPace: 2500,
+
+	//Timeouts container
+	timeouts: {},
+
+	//Attribute and Function "Constants"
+	attributes: ['href', 'class'],
+	properties: ['checked', 'selected', 'nodeType'],
+	functionsVoid: ['hide', 'show'],
+	functionsSingle: ['addClass', 'after', 'append', 'appendTo', 'before', 'detach', 'empty', 'fadeIn', 'fadeOut', 'height', 'html', 'insertAfter', 'insertBefore', 'prepend', 'prependTo', 'remove', 'removeAttr', 'removeClass', 'removeData', 'removeProp', 'replaceWidth', 'scrollLeft', 'scrollTop', 'text', 'toggleClass', 'unwrap', 'val',  'width', 'wrap', 'wrapAll', 'wrapInner'],
+	functionsMulti: ['animate', 'attr', 'fadeTo', 'prop'],
+
+	//Utility functions
+	in_array: function(n, h){
+		for(var i in h){
+			if(h[i] == n)
+				return true;
+		}
+		return false;
+	},
+	array_remove: function(t, s){
+		for(var i in s){
+			if(t == s[i])
+				s.splice(i, 1);
+		}
+		return s;
+	},
+
+	//Log functions
+	logMax: 500,
+	log: function(data, force){
+		if(this.actionLog.length > this.logMax){
+			this.actionLog.push('');
+			this.actionLog = this.actionLog.slice(-1 * this.logMax,-1);
+		}
+		this.actionLog.push(data);
+
+		if(force || (this.logging && console.log !== undefined)) console.log(data);
+	},
+
+	//Retry Function
+	retry: function(action, data, skipLogging){
+		for(var p in Marionette.timeouts){
+			clearTimeout(Marionette.timeouts[p]);
+		}
+
+		Marionette.tries++;
+		this.log('Error connecting, retry number '+Marionette.tries, true);
+		Marionette.timeouts[action] = setTimeout(function(){
+			Marionette.sendData(action, data, skipLogging);
+		}, Marionette.retryPace * (Math.floor(Marionette.tries / 10) + 1));
+	},
+
+	//The send data function
+	sendData: function(action, data, skipLogging){
+		var m = this;
+		var $ = jQuery;
+
+		m.sendCount++;
+
+		m.log('Send #'+this.sendCount);
+		m.log('Session Key: '+this.sessionKey);
+
+		var request;
+		if(typeof data == 'undefined')
+			request = {action: action, k: this.sessionKey};
+		else
+			request = {action: action, k: this.sessionKey, data: data === null ? m.defaultData : data};
+
+		m.log('Sending the following data to '+m.script+':');
+		m.log(request);
+
+		var start = (new Date()).getTime(), end;
+
+		$.ajax({
+			url: m.script,
+			data: {Marionette: request},
+			dataType: 'json',
+			type: 'POST',
+			success: function(response){
+				end = (new Date()).getTime();
+
+				if(m.tries > 0){
+					m.log('Connection reestablish after '+m.tries+' tries', true);
+					m.tries = 0;
+				}
+
+				if(response !== null && response.l !== undefined)
+					m.logging = response.l;
+
+				if(!response || typeof response != 'object'){
+					m.log('Data returned is not JSON',true);
+					m.log(response, true);
+					m.retry(action, data, skipLogging);
+					return;
+				}
+
+				m.log('Data Returned:');
+				m.log(response);
+
+				if(response.k !== undefined){
+					m.log('Setting session key: '+response.k);
+					m.sessionKey = response.k;
+				}
+
+				if(response.c !== undefined){
+					var c, t, e, p;
+						c = response.c;
+					m.log('Procession Changes');
+					for(t in c){ // t = target
+						m.log('Begin Processing '+t);
+
+						if(t.match(/^\d+$/)){
+							//Data is being sent to a predefined "mode" function
+							m.log(t+' is numeric, calling custom parser function matching that ID with the following data: "'+c[t]+'"');
+							if(typeof m.parsers[t] == 'function'){//Make sure the function exists
+								m.parsers[t](c[t], t);
+							}else{
+								m.log('No function at parsers['+t+'] exists');
+							}
+						}else{
+							m.log(t+' is a selector, running through attribute settings');
+							e = $(t);
+							if(e.length > 0){
+								for(p in c[t]){ // p = property
+									m.log('Editing "'+p+'" for "'+t+'" with the folowing data: "'+c[t][p]+'"');
+									m.process(p, c[t][p], e);
+								}
+							}else{
+								m.log('No elements matching "'+t+'" were found.');
+							}
+						}
+					}
+				}
+
+				if(response.u !== undefined && response.r === true){
+					m.log('Interval set and repeat is true, setting timeout to repeat "'+action+'" action to "'+m.script+'"');
+
+					m.timeouts[action] = setTimeout(function(){
+						m.sendData(action, data, skipLogging);
+					}, response.u);
+				}else if(action == 'ping'){
+					//Reping the server anyway in 1 minute, just in case
+					m.timeouts[action] = setTimeout(function(){
+						m.sendData(action, data, skipLogging);
+					}, 60000);
+				}
+
+				m.log('Client Execution Time: '+(end-start)+'ms');
+				m.log('Server Execution Time: '+response.t+'ms');
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				end = (new Date()).getTime();
+				m.log(jqXHR, true);
+				m.retry(action, data, skipLogging);
+			}
+		});
+	},
+
+	init: function(script, data){
+		if(script !== undefined) this.script = script;
+		this.sendData('ping', data);
+	},
+
+	process: function(p, v, e){
+		var f, m = this;
+		if(p.match(/data-/) || m.in_array(p, m.attributes)){
+			//Updating a registered attritube
+			m.log(p+' is a registered attribute, editing via $.fn.attr');
+			e.attr(p, v);
+		}else if(m.in_array(p, m.properties)){
+			//Updating a registered property
+			m.log(p+' is a registered property, editing via $.fn.prop');
+			e.prop(p, v);
+		}else if(m.in_array(p, m.functionsVoid)){
+			//Updating a registered jQuery function
+			m.log(p+' is a registered void function, calling function directly');
+			e[p]();
+		}else if(m.in_array(p, m.functionsSingle)){
+			//Updating a registered jQuery function
+			m.log(p+' is a registered single argument function, calling function directly');
+			e[p](v);
+		}else if(m.in_array(p, m.functionsMulti)){
+			//Updating a registered multi argument jQuery function
+			//(value is assumed to be array of arguments)
+			m.log(p+' is a registered multi argument function, calling function through apply');
+			$.fn[p].apply(e, v);
+		}else{
+			f = m.parsers[p];
+			if(typeof f == 'function'){
+				m.log(p+' is a custom parser function, calling directly');
+				m.parsers[p](p, v, e, m);
+			}else if(typeof m.parsers[f] == 'function'){
+				m.log(p+' is an alias to the custom parser function '+f+', calling directly');
+				m.parsers[f](p, v, e, m);
+			}else{
+				m.log(p+' is assumed to be a DOM property, editing directly');
+				e.get(0)[p] = v;
+			}
+		}
+	},
+
+	//Parsers
+	parsers: {
+		0: function(data){
+			Marionette.log('Data returned, logging data.');
+			Marionette.log(data,true);
+		},
+		1: function(data){
+			if(typeof data == 'object'){
+				/*
+				Data is (presumably) an array of multiple
+				messages to log, so go through each one and
+				log them.
+				*/
+				Marionette.log('Data returned is an object, assuming it\'s an array of messages to alert with.');
+				for(var m in data){
+					alert(data[m]);
+				}
+			}else{//data is just a string (single message), log it.
+				Marionette.log('Data returned is a string, alerting with data.');
+				alert(data);
+			}
+		},
+		css: function(p, v, e){
+			for(var k in v){
+				$(e).css(k, v[k]);
+			}
+		},
+		data: function(p, v, e){
+			for(var k in v){
+				$(e).data(k, v[k]);
+			}
+		},
+		traverse: function(p, v, e, m){
+			var s;
+			if(v.length == 2){
+				s = v[0];
+				v = v[1];
+			}else{
+				s = null;
+			}
+			for(var k in v){
+				m.process(k, v[k], $(e)[p](s));
+			}
+		},
+		children: 'traverse',
+		eq: 'traverse',
+		fitler: 'traverse',
+		find: 'traverse',
+		has: 'traverse',
+		is: 'traverse',
+		next: 'traverse',
+		nextAll: 'traverse',
+		nextUntil: 'traverse',
+		not: 'traverse',
+		parent: 'traverse',
+		parents: 'traverse',
+		parentsUntil: 'traverse',
+		prev: 'traverse',
+		prevAll: 'traverse',
+		prevUntil: 'traverse',
+		siblings: 'traverse'
+	}
+};
---
+++ Marionette.php
@@ -0,0 +1,372 @@
+<?php
+/*
+	Marionette is a 2 pronged system for manipulating the DOM.
+	This PHP class is used to build an array of instructions
+	for the JS class to follow and make changes to the DOM.
+
+	Instructions are sent in this basic structure:
+	{
+		target: {
+			property: value
+		}
+	}
+
+	target =	a jQuery selector (to directly edit an element)
+				or a number to call a predefined parser function
+				within the JS class.
+				(set via Marionette.parsers[N] = function(){//do something})
+
+	property =	a DOM property or attribute to manipulate through
+				jQuery or directly through the DOM object. This can
+				also be the name of a jQuery function such as addClass,
+				or a predefined parser function.
+				(set via Marionette.parsers.X = function(){//do something})
+
+	value =		mixed data, such as a boolean, a string, or an object.
+				For jQuery functions that take multiple arguments, pass
+				an array of the arguments.
+				(set via Marionette.functions[Single|Multi].X = function(){//do something})
+
+	These instructions are then sent back in the form of a JSON object,
+	which is composed of the following:
+	{
+		u: <update interval>
+		r: <repeat boolean>
+		l: <logging boolean>
+		k: <session key>
+		c: <instrution data>
+		t: <execution time>
+	}
+
+	update interval =	How long for the JS class to wait before sending
+						again, assuming repeat is TRUE
+
+	repeat boolean =	Wether or not to repeat this action again; this
+						is for something like a ping action that regularly
+						checks if there are new changes available
+
+	logging boolean =	Wether or not to log debug messages in the console,
+						useful in tracking down what data was sent back and
+						ensuring it's processed properly
+
+	session key =		The key of Marionette's $_SESSION entry that this
+						particulat connection is related to.
+
+	instruction data =	The instructions for what changes to make to the DOM
+
+	execution time =	An estimate of how long it took the server to execute
+						this, used in the debug logs if logging is true.
+
+	Marionette's uses a section of the $_SESSION variable to store
+	information such as previously sent instructions so as to cut out
+	redundant changes (unless they are marked as forced, in which
+	case it will send that change no matter what).
+
+	Marionette's session variable supports multiple instances; if
+	the user has multiple windows open, the changes will only affect
+	the specific window. In an attempt to prevent memory hogging,
+	Marionette regularly clears out sessions that haven't been touched
+	for the last 60 seconds.
+*/
+ini_set('session.use_cookies', 1);
+ini_set('session.use_only_cookies', 1);
+
+class Marionette
+{
+	public $slowpings = false; //specify that pings should be a slow as possible
+	public $repeat = false; //specify that pings for this should be repeated
+	public $append = false; //Default append setting
+	public $forced = false; //Default force setting
+	public $logging = false; //Default js log setting
+	public $debug = false; //specify debug mode (current testing server doesn't allow `update`)
+
+	public $actions = array(); //List of function calls for each action
+	public $data = array(); //For collecting multiple replies to send
+	public $force = array(); //For forcing changes on any targets logged here.
+
+	public $start; //The start time of when Marionette was initialized
+	public $key; //The session entry key that holds relevant Marionette data
+
+	public $fixed_interval = false; //Whether or not to go with fixed update interval (should be for dev purposes only)
+	public $update_interval = 1000; //How long to wait before repeating if repeat is TRUE
+	public $min_update_interval = 0; //The minimum update interval, added to the actual upate_interval
+
+	function make_key(){
+		//Set the key to either be the one passed in the AJAX call, or create one based on their IP and the microtime
+		$this->key = !empty($_REQUEST['Marionette']['k']) && $_REQUEST['Marionette']['k'] != 'null' ? $_REQUEST['Marionette']['k'] : md5($_SERVER['REMOTE_ADDR']).md5(microtime());
+
+
+		//Check if Marionette data is setup for this session, setup with birth time if not
+		if(!isset($_SESSION['Marionette'][$this->key]) || !is_array($_SESSION['Marionette'][$this->key]))
+			$_SESSION['Marionette'][$this->key] = array(
+				'birth' => time()
+			);
+
+		//Set/Update the touched timestamp on this session to renew it
+		$_SESSION['Marionette'][$this->key]['touched'] = time();
+	}
+
+	function clean_session(){
+		//Run through all sessions and see if any are older than 60 seconds, delete if so
+		foreach($_SESSION['Marionette'] as $key => $session){
+			if($key != $this->key && isset($session['touched']) && time() - $session['touched'] > 60)
+				unset($_SESSION['Marionette'][$key]);
+		}
+	}
+
+	function __construct($args){		
+		$this->start = microtime(true);
+
+		$this->make_key();
+		$this->clean_session();
+
+		$this->sysLoadTime();
+
+		foreach($args as $arg => $value){
+			$this->$arg = $value;
+		}
+
+		//If Marionette data is sent to the server, process it based on the action parameter
+		if(isset($_REQUEST['Marionette'])){
+			header('HTTP/1.0 200 OK');
+			//If no action is actually passed, send log to the browser
+			if(!isset($_REQUEST['Marionette']['action'])) $this->sendData(0, "No action specified", true, false);
+
+			$action = $_REQUEST['Marionette']['action'];
+
+			$data = null;
+			if(isset($_REQUEST['Marionette']['data'])) $data = $_REQUEST['Marionette']['data'];
+
+			if(isset($this->actions[$action])){
+				//An processor function for this action is set, call the function.
+				$func = $this->actions[$action];
+				$func($this, $data, $action);
+				//call_user_func($this->actions[$action], &$this, $data, $action);
+			}elseif(isset($this->actions['default'])){
+				//A defualt action process is set, call it.
+				$func = $this->actions['default'];
+				$func($this, $data, $action);
+				//call_user_func($this->actions['default'], &$this, $data, $action);
+			}else{
+				//Dammit, nothing, send a log to the browser
+				$this->call(0, "Unrecognized action: $action, no default function set.", true);
+			}
+			$this->reply();
+		}
+	}
+
+	// Primary Data Return; echos out the JSON data;
+	function reply($data = true){
+		$data = $data === true ? $this->data : $data;
+
+		//Load the relevant session changes data
+		$session = &$_SESSION['Marionette'][$this->key]['changes'];
+
+		//Run through the changes and compare to the $_SESSION copy
+		//to see if any of the changes are new compared to last request.
+		foreach($data as $target => $change){
+			if(	isset($session[$target]) &&
+				json_encode($session[$target]) === json_encode($change) &&
+				!in_array($target, $this->force)){
+				//If the target is the same, AND there's no force change
+				//enabled for that target, unset it, lightening $data
+				unset($data[$target]);
+			}else{
+				$session[$target] = $change;
+			}
+		}
+
+		//Print out the JSON data
+		echo json_encode(array(
+			'u' => $this->update_interval + $this->min_update_interval,
+			'r' => $this->repeat,
+			'l' => $this->logging,
+			'k' => $this->key,
+			'c' => $data,
+			't' => round(microtime(true) - $this->start, 4)
+		));
+
+		exit; //Adding anything after the JSON will break it.
+	}
+
+	// Update a target with multiple properties
+	function update($target, $data, $force = null, $append = null){
+		if($force === null) //set $force to the default setting
+			$force = $this->forced;
+
+		if($force) //Add target to list of forced changes
+			$this->force[] = $target;
+
+		if($append === null) //set $append to the default setting
+			$append = $this->append;
+
+		if($append){
+			//Add it to the $data array
+			if(is_array($this->data[$target])){
+				//changes for this target exist, add/ovewrite property
+				$this->data[$target][$prop] = $option;
+			}else{
+				//create new $data entry for this target
+				$this->data[$target] = array(
+					$prop => $option
+				);
+			}
+		}else{
+			//Immediately reply with the passed data
+			$this->reply(
+				array(
+					$target => $data
+				)
+			);
+		}
+	}
+
+	// Update a specific property
+	function updateProp($target, $prop, $value, $force = null, $append = null, $bool = false){
+		if($bool){ //property must be TRUE or FALSE
+			if($value == 1) $value = true;
+			else $value = false;
+		}
+
+		if($force === null) //set $force to the default setting
+			$force = $this->forced;
+
+        if($force) //Add target to list of forced changes
+            $this->force[] = $target;
+
+		if($append === null) //set $append to the default setting
+			$append = $this->append;
+
+		if($append){
+			//Add it to the $data array
+			if(isset($this->data[$target]) && is_array($this->data[$target])){
+				//changes for this target exist, add/ovewrite property
+				$this->data[$target][$prop] = $value;
+			}else{
+				//create new $data entry for this target
+				$this->data[$target] = array(
+					$prop => $value
+				);
+			}
+		}else{
+			//Immediately reply with the passed data
+			$this->reply(
+				array(
+					$target => array(
+						$prop => $value
+					)
+				)
+			);
+		}
+	}
+
+	// Call a predefined numeric parser
+	function call($key, $data, $force = null, $append = null){
+		if($force === null) //set $force to the default setting
+			$force = $this->forced;
+
+        if($force) //Add target to list of forced changes
+            $this->force[] = $key;
+
+		if($append === null) //set $append to the default setting
+			$append = $this->append;
+
+		if($append){
+			//Add it to the $data array
+			$this->data[$key] = $data;
+		}else{
+			//Immediately reply with the passed data
+			$this->reply(
+				array(
+					$key => $data
+				)
+			);
+		}
+	}
+
+	// Update multiple targets in one go
+	function bulkUpdate(array $targets, $force = null, $append = null){
+		if($force === null) //set $force to the default setting
+			$force = $this->forced;
+
+		if($force) //Add targets to list of forced changes
+			foreach($targets as $target => $data){
+				$this->force[] = $target;
+			}
+
+		if($append === null) //set $append to the default setting
+			$append = $this->append;
+
+		if($append){
+			//Add it to the $data array
+			foreach($targets as $target => $data){
+				if(is_array($this->data[$target])){
+					//changes for this target exist, add/ovewrite properties
+					foreach($target as $prop => $value){
+						$this->data[$target][$prop] = $value;
+					}
+				}else{
+					//create new $data entry for this target
+					$this->data[$target] = $data;
+				}
+			}
+		}else{
+			//Immediately reply with the passed data
+			foreach($targets as $target => $data){
+				$reply[] = array(
+					$target => $data
+				);
+			}
+			$this->reply($reply);
+		}
+	}
+
+	// Helper Function: system load time
+	function sysLoadTime(){
+		if($this->fixed_interval){
+			$this->system_load = '?';
+		}elseif($this->debug){
+			$this->update_interval = 1000;
+			$this->system_load = '?';
+		}else{
+			if(@file_exists('/proc/loadavg')){
+				$load = explode(' ', file_get_contents('/proc/loadavg'));
+				$load = (float) $load[0];
+			}elseif(function_exists('shell_exec')){
+				$load = explode(' ', `uptime`);
+				$load = (float) $load[0];
+			}else{
+				$load = 1;
+			}
+
+			$this->system_load = $load;
+
+			if($load <= 1)
+				$u = mt_rand(1000, 2000);
+			elseif($load <= 1.5)
+				$u = mt_rand(1000, 2000);
+			elseif($load <= 2.5)
+				$u = mt_rand(1500, 2500);
+			elseif($load <= 3)
+				$u = mt_rand(2000, 3000);
+			elseif($load <= 3.25)
+				$u = mt_rand(2500, 3500);
+			elseif($load <= 3.45)
+				$u = mt_rand(3500, 4500);
+			elseif($load <= 3.55)
+				$u = mt_rand(4500, 5500);
+			elseif($load <= 3.65)
+				$u = mt_rand(5500, 6500);
+			elseif($load <= 3.75)
+				$u = mt_rand(6500, 7500);
+			elseif($load <= 3.85)
+				$u = mt_rand(7500, 8500);
+			elseif($load <= 3.95)
+				$u = mt_rand(9000, 11000);
+			else
+				$u = mt_rand(19000, 21000);
+
+			$this->update_interval = $u;
+		}
+	}
+}
