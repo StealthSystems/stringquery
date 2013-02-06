2e08b17962f934b4eff6eef9309e8fbad00aa504
Doug Wollison <doug@wollison.net>
2013-02-06 10:28:53 -0500

    Updated version number and release date.

--- StringQuery.js
+++ StringQuery.js
@@ -1,7 +1,7 @@
 /*
 Built with StringQuery (https://github.com/dougwollison/StringQuery/)
 
-Version 1.1.0; Released 01/02/2013
+Version 1.1.1; Released 06/02/2013
 
 Copyright © 2012 Richard Cornwell & Doug Wollison
 For conditions of distribution and use, see copyright notice in LICENSE
